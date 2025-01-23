c6b7b800b809957734a3ad43192a8ba6dd2233ff
Tasnim <tasnimalbrashdi@gmail.com>
2025-01-23 10:38:36 +0400

    save

--- Services/IDepartmentService.cs
+++ Services/IDepartmentService.cs
@@ -10,6 +10,6 @@ namespace HospitalSystemTeamTask.Services
         void UpdateDepartment(DepDTO departmentDto);
         void SetDepartmentActiveStatus(int departmentId, bool isActive);
         Department GetDepartmentByName(string department);
-        Department GetDepartmentByid(int did);
+        DepDTO GetDepartmentByid(int departmentId);
     }
 }
