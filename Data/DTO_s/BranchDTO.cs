99a24283857a6ef727cfa2394e85150e84c5dd13
Azzak <azzaalmahrooqi79@gmail.com>
2025-01-26 09:40:25 +0400

    Manage Branch Page

--- Data/DTO_s/BranchDTO.cs
+++ Data/DTO_s/BranchDTO.cs
@@ -4,6 +4,8 @@ namespace HospitalSystemTeamTask.DTO_s
 {
     public class BranchDTO
     {
+        public int BID { get; set; } // Add this property for the Branch ID
+
         [Required(ErrorMessage = "Branch name is required.")]
         [StringLength(100, ErrorMessage = "Branch name must be between 3 and 100 characters.", MinimumLength = 3)]
         public string BranchName { get; set; }
@@ -11,8 +13,12 @@ namespace HospitalSystemTeamTask.DTO_s
         [Required(ErrorMessage = "Location is required.")]
         [StringLength(100, ErrorMessage = "Location must be between 3 and 100 characters.", MinimumLength = 3)]
         public string Location { get; set; }
-        public bool BranchStatus { get; set; }
 
-     
+        public bool BranchStatus { get; set; }
     }
+}
+public class UpdateBranchDTO
+{
+    public string BranchName { get; set; }
+    public string Location { get; set; }
 }
