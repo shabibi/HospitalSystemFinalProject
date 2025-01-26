365dd721a3b738d15da6268bd261b11967a02875
Afra Alshabibi <a.shabibi20@gmail.com>
2025-01-26 11:27:38 +0400

    Adding Authentication service.

---
+++ Helper/JwtTokenResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace HospitalSystemTeamTask.Helper
+{
+    public class JwtTokenResponse
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
---
+++ Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using HospitalSystemTeamTask.Helper;
+using HospitalSystemTeamTask.Models;
+
+namespace HospitalSystemTeamTask.Services
+{
+    public interface IAuthService
+    {
+        JwtTokenResponse GenerateToken(User user);
+        Task SaveTokenToCookie(string token);
+    }
+}
