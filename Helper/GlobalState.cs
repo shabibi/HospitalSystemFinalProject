7443e218b8d7f687e926053356df23e39d444efe
Afra Alshabibi <a.shabibi20@gmail.com>
2025-01-28 15:25:19 +0400

    Add GlobalState

---
+++ Helper/GlobalState.cs
@@ -0,0 +1,13 @@
+﻿namespace HospitalSystemTeamTask.Helper
+{
+
+        // File: GlobalState.cs
+        public static class GlobalState
+        {
+            public static int UID { get; set; }
+            public static string Name { get; set; }
+            public static bool IsAuthenticated { get; set; }
+        }
+
+
+}
