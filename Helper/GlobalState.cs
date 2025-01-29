dd79520609177c2d77cd914a526c6c0c8bafb542
Azzak <azzaalmahrooqi79@gmail.com>
2025-01-29 10:30:53 +0400

    final upload to chack sign in / sign out , then patien profile

--- Helper/GlobalState.cs
+++ Helper/GlobalState.cs
@@ -1,13 +1,10 @@
 ﻿namespace HospitalSystemTeamTask.Helper
 {
-
-        // File: GlobalState.cs
-        public static class GlobalState
-        {
-            public static int UID { get; set; }
-            public static string Name { get; set; }
-            public static bool IsAuthenticated { get; set; }
-        }
-
-
+    public static class GlobalState
+    {
+        public static int UID { get; set; }
+        public static string Name { get; set; }
+        public static bool IsAuthenticated { get; set; }
+        public static string Role { get; set; } // E.g., "Patient", "Doctor", "Staff"
+    }
 }
