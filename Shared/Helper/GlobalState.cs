cbcb5592e593a8e4668ffdd24308a50ed91192be
shabibi <a.shabibi20@gmail.com>
2025-02-11 15:39:34 +0400

    change the nav menu to be dinamic and delete multi layout

--- Shared/Helper/GlobalState.cs
+++ Shared/Helper/GlobalState.cs
@@ -4,7 +4,14 @@
     {
         public static int UID { get; set; }
         public static string Name { get; set; }
-        public static bool IsAuthenticated { get; set; }
+        public static bool IsAuthenticated { get; set; } = false; 
         public static string Role { get; set; } // E.g., "Patient", "Doctor", "Staff"
+
+        public static event Action OnAuthStateChanged;
+
+        public static void NotifyAuthStateChanged()
+        {
+            OnAuthStateChanged?.Invoke();
+        }
     }
 }
